490f96562ad8249250c1fe79babf306ab69b666c
Dino Cosic <102690675+dino-cosic@users.noreply.github.com>
2025-06-04 01:38:02 +0200

    Update start-agent.sh

--- .devcontainer/start-agent.sh
+++ .devcontainer/start-agent.sh
@@ -6,15 +6,20 @@ if [ -z "$AZP_URL" ] || [ -z "$AZP_TOKEN" ] || [ -z "$AZP_POOL" ] || [ -z "$AZP_
   exit 1
 fi
 
-echo "Configuring Azure DevOps agent..."
+# Configure agent only if not already configured
+if [ ! -f .agent ]; then
+  echo "Configuring Azure DevOps agent..."
+  
+  ./config.sh --unattended \
+    --url "$AZP_URL" \
+    --auth pat \
+    --token "$AZP_TOKEN" \
+    --pool "$AZP_POOL" \
+    --agent "$AZP_AGENT_NAME" \
+    --replace \
+    --acceptTeeEula
+fi
 
-./config.sh --unattended \
-  --url "$AZP_URL" \
-  --auth pat \
-  --token "$AZP_TOKEN" \
-  --pool "$AZP_POOL" \
-  --agent "$AZP_AGENT_NAME" \
-  --acceptTeeEula
-
-echo "Starting agent..."
-./run.sh
+echo "Starting agent in the background..."
+# Start in background and persist through shell exits
+nohup ./run.sh > agent.log 2>&1 &
